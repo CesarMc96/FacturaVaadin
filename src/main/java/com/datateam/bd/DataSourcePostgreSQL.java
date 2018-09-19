cb8856c5a132b5be7f9e72fc1744f8f37b5161a5
CesarMC96 <>
2018-09-19 11:30:00 -0500

    Ya sirve postgresql

--- src/main/java/com/datateam/bd/DataSourcePostgreSQL.java
+++ src/main/java/com/datateam/bd/DataSourcePostgreSQL.java
@@ -1,4 +1,3 @@
-
 package com.datateam.bd;
 
 import java.sql.Connection;
@@ -17,19 +16,23 @@ public class DataSourcePostgreSQL implements DataSource {
     private ArrayList arreglo;
 
     public DataSourcePostgreSQL() {
+        System.out.println("Entro");
         connection = null;
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://192.168.1.252/jul2818", "recetas", "datateam");
+            connection = DriverManager.getConnection("jdbc:postgresql://192.168.1.253:5432/jul2818", "recetas", "datateam");
             this.st = this.connection.createStatement();
+            System.out.println("Opened database successfully");
         } catch (ClassNotFoundException ex) {
+            System.out.println("Error al conectarse a la base de datos.");
         } catch (SQLException ex) {
             Logger.getLogger(DataSourcePostgreSQL.class.getName()).log(Level.SEVERE, null, ex);
             System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
         }
-        System.out.println("Opened database successfully");
+
     }
-    
+
+    @Override
     public Object ejecutarConsulta(String consulta) {
         System.out.println(consulta);
         ResultSet rs = null;
@@ -37,11 +40,10 @@ public class DataSourcePostgreSQL implements DataSource {
             rs = st.executeQuery(consulta);
             System.out.println(rs);
 
-//            while (rs.next()) {
-//                String nombre = rs.getString(0);
-//                System.out.println(nombre);
-//            }
-
+            while (rs.next()) {
+                String nombre = rs.getString(1);
+                System.out.println(nombre);
+            }
             rs.close();
             st.close();
             connection.close();
@@ -50,5 +52,5 @@ public class DataSourcePostgreSQL implements DataSource {
         }
         return (Object) rs;
     }
-    
+
 }
--- src/main/java/com/datateam/bd/MyUI.java
+++ src/main/java/com/datateam/bd/MyUI.java
@@ -1,7 +1,6 @@
 package com.datateam.bd;
 
 import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
@@ -14,13 +13,6 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import java.time.LocalDate;
 
-/**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of an HTML page where a Vaadin application is embedded.
- * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
- */
 @Theme("mytheme")
 public class MyUI extends UI {
 
@@ -35,14 +27,18 @@ public class MyUI extends UI {
 
         btn.addClickListener(e -> {
             System.out.println(date.getValue());
+            System.out.println("Hola");
             DataSourcePostgreSQL s = new DataSourcePostgreSQL();
-            s.ejecutarConsulta("SELECT id FROM vista_factura_por_compra_plan;");
+            s.ejecutarConsulta("SELECT id, fecha_factura, folio_fiscal, fecha_mx, nombre_producto, medico, correo\n" +
+"  FROM public.vista_factura_por_compra_plan;");
         });
-        
+
+//        Label l = new Label("HOLA");
         Grid g = new Grid();
         layout.addComponents(date);
         layout.addComponent(btn);
-        layout.addComponent(g);
+        layout.addComponent(g);        
+//        layout.addComponent(l);
         setContent(layout);
     }
 
